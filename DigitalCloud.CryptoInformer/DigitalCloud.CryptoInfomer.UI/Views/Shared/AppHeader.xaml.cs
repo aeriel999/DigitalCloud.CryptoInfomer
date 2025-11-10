ec6fc261af594c18159a495b35c5e42c52acc37c
aeriel999 <ok4743@gmail.com>
2025-11-10 12:48:25 +0200

    efault Light theme + two-state Light/Dark toggle via ThemeMode

--- DigitalCloud.CryptoInformer/DigitalCloud.CryptoInfomer.UI/Views/Shared/AppHeader.xaml.cs
+++ DigitalCloud.CryptoInformer/DigitalCloud.CryptoInfomer.UI/Views/Shared/AppHeader.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace DigitalCloud.CryptoInfomer.UI.Views.Shared
 {
@@ -10,10 +11,21 @@ namespace DigitalCloud.CryptoInfomer.UI.Views.Shared
         public AppHeader()
         {
             InitializeComponent();
-           
+
+            Loaded += (_, __) =>
+            {
+                ThemeSwitch.IsChecked = Application.Current.ThemeMode == ThemeMode.Dark;
+            };
+            ThemeSwitch.Click += ThemeSwitch_Click;
         }
 
-//ToDo Switch theme mode        
+        private void ThemeSwitch_Click(object? sender, RoutedEventArgs e)
+        {
+#pragma warning disable WPF0001
+            var app = Application.Current;
+            app.ThemeMode = (ThemeSwitch.IsChecked == true) ? ThemeMode.Dark : ThemeMode.Light;
+#pragma warning restore WPF0001
+        }
     }
 
 
